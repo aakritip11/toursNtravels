fbf04c41d204ea95bbb29aa96d35850868eb0bff
aakritip11 <118073835+aakritip11@users.noreply.github.com>
2023-07-17 20:19:48 +0530

    Update TourDetails.jsx

--- src/Components/Tours/TourDetails.jsx
+++ src/Components/Tours/TourDetails.jsx
@@ -2,22 +2,17 @@ import React, { useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
-import { HiOutlineLocationMarker, HiOutlineClipboardCheck } from 'react-icons/hi';
+import './tours1.css'
+import Tours, {Data} from '../../Components/Tours/Tours';
 
-const TourDetails = ({ data }) => {
+const TourDetails = () => {
   const { id } = useParams();
 
   useEffect(() => {
     Aos.init({ duration: 2000 });
   }, []);
 
-  let tour = null;
-  for (let i = 0; i < data.length; i++) {
-    if (data[i].id === parseInt(id)) {
-      tour = data[i];
-      break;
-    }
-  }
+  const tour = Data.find((item) => item.id === parseInt(id));
 
   const navigate = useNavigate();
 
@@ -27,27 +22,27 @@ const TourDetails = ({ data }) => {
 
   return (
     <section className="tour-details container">
-      <div className="tour-info">
+      <div className="tour-info" data-aos="fade-up">
         <h2>{tour.destTitle}</h2>
         <div className="tour-details-content">
           <div className="tour-image" data-aos="fade-up">
-            <img src={tour.imgSrc} alt={tour.destTitle} />
+            <img src={tour.imgSrc} alt='' />
           </div>
           <div className="tour-description" data-aos="fade-up">
             <h3>{tour.location}</h3>
             <p>{tour.description}</p>
             <div className="tour-details-footer">
-              <div className="tour-grade">
+              {/* <div className="tour-grade">
                 <span>{tour.grade}</span>
-              </div>
+              </div> */}
               <div className="tour-price">
-                <h5>{tour.fees}</h5>
+                <h5>Fees: {tour.fees}</h5>
               </div>
             </div>
           </div>
         </div>
         <button className="btn flex" onClick={() => navigate('/tours')}>
-          BACK <HiOutlineClipboardCheck className="icon" />
+          BACK 
         </button>
       </div>
     </section>
